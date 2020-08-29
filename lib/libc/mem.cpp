6e61702a02038b50d10bc7088695f3fcad9a838e
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2020-08-28 21:08:51 -0400

    Starting mem.cpp

--- lib/libc/mem.cpp
+++ lib/libc/mem.cpp
@@ -2,7 +2,19 @@
 
 namespace mem {
 
+void memcpy(uint8_t *source, uint8_t *dest, int nbytes){
+    int i;
+    for (i = 0; i < nbytes; i++) {
+        *(dest + i) = *(source + i);
+    }
+}
+void memset(uint8_t *dest, uint8_t val, uint32_t len){
+    uint8_t *temp = (uint8_t *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
+}
 
-uint32_t kmalloc(){}
+
+
+
 
 } // namespace mem
--- lib/libc/mem.h
+++ lib/libc/mem.h
@@ -1,3 +1,5 @@
+#ifndef MEM_H
+#define MEM_H
 #include <stdint.h>
 #include <stddef.h>
 
@@ -7,21 +9,19 @@ namespace mem {
 
 
 
-void memory_copy(uint8_t *source, uint8_t *dest, int nbytes);
-void memory_set(uint8_t *dest, uint8_t val, uint32_t len);
+struct block {
+    uint8_t state;
+    uint32_t size;
+    uint32_t next;
+};
 
-uint32_t free_mem_addr = KERNEL_START_POINT;
+void memcpy(uint8_t *source, uint8_t *dest, int nbytes);
+void memset(uint8_t *dest, uint8_t val, uint32_t len);
 
-uint32_t blockHeaderSize = 0x1;
 
-uint32_t free_mem_addr = KERNEL_START_POINT;
+uint32_t createBlock(char *data);
 
-uint32_t kmalloc(size_t, int align, uint32_t *phys_addr);
 
-void kfree (void* ptr);
-
-uint32_t virtualToPhysical(uint32_t virt);
-
-uint32_t physicalToVirtual(uint32_t virt);
 
 } // namespace mem
+#endif
