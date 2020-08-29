3f3e1cb84a54b05b312dc1f39c8d65365b0d078e
riley <meetingprince34@outlook.com>
2020-08-28 22:02:10 -0400

    Started Screen Scrolling.

--- lib/libc/mem.cpp
+++ lib/libc/mem.cpp
@@ -1,20 +1,28 @@
 #include "mem.h"
 
+
 namespace mem {
 
-void memcpy(uint8_t *source, uint8_t *dest, int nbytes){
+void memcpy(uint32_t *source, uint32_t *dest, int nbytes){
     int i;
     for (i = 0; i < nbytes; i++) {
         *(dest + i) = *(source + i);
     }
 }
-void memset(uint8_t *dest, uint8_t val, uint32_t len){
-    uint8_t *temp = (uint8_t *)dest;
+void memset(uint32_t *dest, uint32_t val, uint32_t len){
+    uint32_t *temp = (uint32_t *)dest;
     for ( ; len != 0; len--) *temp++ = val;
 }
 
 
 
-
+uint32_t createBlock(char data[defaultBlockSize]){
+    char *s = "";
+    uint32_t so = sizeof(data);
+    string::int_to_ascii(so,s);
+    screen::kprint(s);
+    screen::kprint((char *)"\n");
+    screen::kprint(data);
+}
 
 } // namespace mem
