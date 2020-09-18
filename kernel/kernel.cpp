2d10caf93a45e509acb66d367377717e87322e5f
riley <meetingprince34@outlook.com>
2020-09-18 12:05:37 -0400

    Now with vectors and a basic iterator class. Based off of: https://www.geeksforgeeks.org/program-to-create-custom-vector-class-in-c

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -9,12 +9,12 @@ void kernel_main() {
     //    chr[i] = 0x72-32;
     //}
     //screen::kprint(chr);
-    screen::kprint((char *)"Lets Test Malloc With a psuedo random number generator\n");
+    screen::kprint((char *)"Lets Test Malloc With a psuedo random number generator.\n");
     
     int i = 5;
-    int n;
+    int n,n2;
     char * buffer;
-    rand::srand(200);
+    rand::srand(1880);
     buffer = (char*) mem::malloc (i+1);
     if (buffer==NULL){
         screen::kprint((char *)"That should not have happened!\n");
@@ -25,12 +25,25 @@ void kernel_main() {
     screen::kprint((char *)"Random String:");
     screen::kprint(buffer);
     screen::kprint((char *)"\n");
+    
+    
+
     mem::free (buffer);
 
 
     
-  }
-  vector v;
-  v.push((char *)"Hello, World!");
+    }
+    screen::kprint((char *)"Lets Test vector with a psuedo random number generator.\n");
+    vector<char *> v;
+    rand::srand(200);
+    for (n=0; n<5; n++){
+      v.push_back((char *)(rand::rand()%26+'a'));
+    }
+    for(int i=0; i<v.size(); i++){
+      screen::kprint(v[i]);
+    }
+
+    screen::kprint((char *)"\n");
+    screen::kprint((char *)"Tests Complete!\n");
 }
 
