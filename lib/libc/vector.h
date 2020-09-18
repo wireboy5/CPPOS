ffc9bb258f0695ab8479c6f36f0cb50886bcf19f
riley <30930649+wireboy5@users.noreply.github.com>
2020-09-18 10:27:00 -0400

    Now has new and delete. Working on vector

---
+++ lib/libc/vector.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#include "op.h"
+
+
+template <class T> class vector {
+
+private:
+    int* arr; 
+    int capacity; 
+    int current; 
+public:
+
+};
+
+#endif
