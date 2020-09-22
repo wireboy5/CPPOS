2f167cff5649d13f5c034ee44c14993691f39f09
riley <30930649+wireboy5@users.noreply.github.com>
2020-09-21 20:28:03 -0400

    New include system.

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -5,11 +5,12 @@
 #define KERNEL_H
 #include <stdint.h>
 #include <stddef.h>
-#include "../modules/drivers/screen.h"
-#include "../lib/libc/mem.h"
-#include "../lib/libc/rand.h"
-#include "../lib/libc/vector.hpp"
-#include "../lib/cpu/arch.h"
+#include <modules/drivers/screen.h>
+#include <lib/libc/mem.h>
+#include <lib/libc/rand.h>
+#include <lib/libc/vector.hpp>
+#include <lib/cpu/arch.h>
+#include <modules/drivers/pci/pci.h>
 
 extern "C" void kernel_main();
 
--- lib/libc/string.cpp
+++ lib/libc/string.cpp
@@ -16,8 +16,43 @@ void int_to_ascii(int n, char str[]) {
 
     reverse(str);
 }
+char * intToASCII(int n){
+    char str[256];
+    int i, sign;
+    if ((sign = n) < 0) n = -n;
+    i = 0;
+    do {
+        str[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
 
+    if (sign < 0) str[i++] = '-';
+    str[i] = '\0';
 
+    reverse(str);
+    return str;
+}
+
+char * hexToASCII(int n){
+    char str[256];
+    append(str, '0');
+    append(str, 'x');
+    char zeros = 0;
+
+    int32_t tmp;
+    int i;
+    for (i = 28; i > 0; i -= 4) {
+        tmp = (n >> i) & 0xF;
+        if (tmp == 0 && zeros == 0) continue;
+        zeros = 1;
+        if (tmp > 0xA) append(str, tmp - 0xA + 'a');
+        else append(str, tmp + '0');
+    }
+
+    tmp = n & 0xF;
+    if (tmp >= 0xA) append(str, tmp - 0xA + 'a');
+    else append(str, tmp + '0');
+    return str;
+}
 
 void hex_to_ascii(int n, char str[]) {
     append(str, '0');
