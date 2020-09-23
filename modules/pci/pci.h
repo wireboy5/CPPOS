37ca6482d3c4a297db74f8dadc7003fbf75658b1
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2020-09-23 18:38:02 -0400

    Starting to restructure PCI config reading.

--- modules/pci/pci.h
+++ modules/pci/pci.h
@@ -18,26 +18,29 @@ public:
     uint16_t vendorID;
     uint16_t status;
     uint16_t command;
-    uint8_t classCode;
-    uint8_t subclass;
-    uint8_t progIF;
-    uint8_t RevisionID;
+    uint16_t classCode;
+    uint16_t subclass;
+    uint16_t progIF;
+    uint16_t RevisionID;
     uint8_t BIST;
     uint8_t headerType;
     uint8_t latencyTimer;
     uint8_t cacheLineSize;
-    void prettyPrint();
+    void prettyPrint(int indent = 4);
+};
+class PCIHeader0 {
+
 };
 
 
 uint16_t pciConfigReadWord (uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 
 
-
 // Check if the device supports provided function
 bool checkFunction(uint8_t bus, uint8_t slot, uint8_t function);
 
 PCIDevice getDevice(uint8_t bus, uint8_t slot, uint8_t function);
+
 }
 
 
